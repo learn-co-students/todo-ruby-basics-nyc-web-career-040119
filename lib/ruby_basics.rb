7604df78e5e3f84033a23f0eaf8d11e024397699
Ferris Boran <ferris.boran@gmail.com>
2019-03-07 18:12:12 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
 def assign_variable(value)
-  
+  return name = value
 end
 
-def argue
-  
+def argue(ment)
+  return ment
 end
 
-def greeting
+def greeting(hey,world)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  return topping
 end
