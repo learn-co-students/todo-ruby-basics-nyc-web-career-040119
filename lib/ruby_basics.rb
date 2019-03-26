fa05248420817e37f9b34c78eb7751eb604669f9
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-03-26 10:34:47 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,29 +1,26 @@
 def division(num1, num2)
-  return num1 / num2
+  num1 / num2
 end
 
-def assign_variable(value)
-  value = "Bob"
+def assign_variable(name)
+  name_variable = name
 end
 
 def argue(phrase)
-return "#{phrase}"
+  phrase
 end
 
 def greeting(greeting, name)
-
 end
 
 def return_a_value
-  return "Nice"
-
+  "Nice"
 end
 
 def last_evaluated_value
-  return "expert"
-
+  "expert"
 end
 
-def pizza_party(ingredient = "cheese")
-return "#{ingredient}"
+def pizza_party(string = "cheese")
+"#{string}"
 end
